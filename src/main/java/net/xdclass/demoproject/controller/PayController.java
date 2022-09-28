047931ff58eff41552928ca85f531044fa6b1f70
yuhosi <yuyuyuhaoshi@163.com>
2022-09-28 23:06:31 +0800

    update

--- src/main/java/net/xdclass/demoproject/controller/PayController.java
+++ src/main/java/net/xdclass/demoproject/controller/PayController.java
@@ -1,6 +1,7 @@
 package net.xdclass.demoproject.controller;
 
 import net.xdclass.demoproject.config.WechatConfig;
+import net.xdclass.demoproject.task.AsyncTask;
 import net.xdclass.demoproject.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @RestController
 @RequestMapping("api/v1/pub/pay")
@@ -16,12 +19,30 @@ public class PayController {
     @Autowired
     private WechatConfig wechatConfig;
 
+    @Autowired
+    private AsyncTask asyncTask;
+
     @GetMapping("test")
-    public JsonData testConfig(){
+    public JsonData testConfig() {
         Map<String, String> map = new HashMap<>();
         map.put("appid", wechatConfig.getPayAppId());
         map.put("secret", wechatConfig.getPaySecret());
         map.put("merchat", wechatConfig.getPayMerchant());
+        asyncTask.task1();
+        String taskResult;
+        Future<String> task2 = asyncTask.task2();
+        for (;;){
+            if (task2.isDone()){
+                try {
+                    taskResult = task2.get();
+                    break;
+                } catch (InterruptedException | ExecutionException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        map.put("future", taskResult);
 
         return JsonData.buildSuccess(map);
     }
