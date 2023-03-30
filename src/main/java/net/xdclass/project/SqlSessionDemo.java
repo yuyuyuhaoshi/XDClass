b1423ebbb63aa5b9d48a706fdb48e596adc24af8
Hosi YU <yuyuyuhaoshi@gmail.com>
2023-03-31 01:58:23 +0800

    feat/mybatis association

--- src/main/java/net/xdclass/project/SqlSessionDemo.java
+++ src/main/java/net/xdclass/project/SqlSessionDemo.java
@@ -1,6 +1,7 @@
 package net.xdclass.project;
 
 import net.xdclass.project.dao.VideoMapper;
+import net.xdclass.project.dao.VideoOrderMapper;
 import net.xdclass.project.domain.Video;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -27,56 +28,11 @@ public class SqlSessionDemo {
         try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
 
             VideoMapper videoMapper = sqlSession.getMapper(VideoMapper.class);
-
-            Video video = videoMapper.selectById(1);
-            System.out.println(video);
-            video.setPrice(1.1);
-            System.out.println(videoMapper.update(video));
-
-            Video video1 = new Video();
-            video1.setId(1);
-            video1.setName("Video1");
-            System.out.println(videoMapper.update(video1));
-
-            System.out.println(videoMapper.selectListByPriceAndName(1.1, "Video1"));
-
+            VideoOrderMapper videoOrderMapper = sqlSession.getMapper(VideoOrderMapper.class);
 
             System.out.println(videoMapper.selectResultMapById(1));
-//            System.out.println(video.toString());
-//
-//            List<Video> videoList = videoMapper.selectList();
-//
-//            System.out.println(videoList.toString());
-//
-//            Video video2 = new Video();
-//            video2.setName("Video4");
-//            video2.setSummary("Summary4");
-//            video2.setCoverImage("CoverImage4");
-//            video2.setPrice(4.0F);
-////            int rows = videoMapper.insert(video2);
-////            System.out.println(rows);
-////            System.out.println(video2);
-////            sqlSession.commit();
-//
-//
-//            Video video3 = new Video();
-//            video3.setName("Video5");
-//            video3.setSummary("Summary5");
-//            video3.setCoverImage("CoverImage5");
-//            video3.setPrice(5.0F);
-//
-//
-//            Video video4 = new Video();
-//            video4.setName("Video6");
-//            video4.setSummary("Summary6");
-//            video4.setCoverImage("CoverImage6");
-//            video4.setPrice(6.0F);
-//
-//            List<Video> videoList1 = new ArrayList<>();
-//            videoList1.add(video3);
-//            videoList1.add(video4);
-//
-//            int row = videoMapper.batchInsert(videoList1);
+            System.out.println( videoOrderMapper.queryList());
+
 
         }
     }
